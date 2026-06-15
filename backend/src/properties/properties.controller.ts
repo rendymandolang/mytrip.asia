8770a618e7bd4240bc532f23609396694eae38e1
Rendy Alexander Mandolang <rendymandolang@gmail.com>
2026-06-15 16:46:06 +0800

    Milestone 2 - Property CRUD completed

--- backend/src/properties/properties.controller.ts
+++ backend/src/properties/properties.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+  Body,
+} from '@nestjs/common';
+
 import { PropertiesService } from './properties.service';
 
 @Controller('properties')
@@ -10,5 +19,33 @@ export class PropertiesController {
   @Get()
   findAll() {
     return this.propertiesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.propertiesService.findOne(Number(id));
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return this.propertiesService.create(body);
+  }
+
+@Delete(':id')
+remove(@Param('id') id: string) {
+  return this.propertiesService.remove(
+    Number(id),
+  );
+}
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: any,
+  ) {
+    return this.propertiesService.update(
+      Number(id),
+      body,
+    );
   }
 }
--- backend/src/properties/properties.service.ts
+++ backend/src/properties/properties.service.ts
@@ -8,4 +8,29 @@ export class PropertiesService {
   async findAll() {
     return this.prisma.property.findMany();
   }
+
+  async findOne(id: number) {
+    return this.prisma.property.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(id: number, data: any) {
+    return this.prisma.property.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async create(data: any) {
+    return this.prisma.property.create({
+      data,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.property.delete({
+      where: { id },
+    });
+  }
 }
